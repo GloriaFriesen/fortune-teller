41a0c133caa9cd3c471f46204678a423d70b8dd2
Dallas Slaughter and Gloria Friesen <gloria@>
2017-02-21 09:44:03 -0800

    Finish JS logic, final product

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,15 @@ $(document).ready(function(){
       var score = 0;
 
       $("input:checkbox[name=bad-luck]:checked").each(function(){
-        score = score - toInt($(this).val());
+        score = score - parseInt($(this).val());
       });
       $("input:checkbox[name=good-luck]:checked").each(function(){
-        score = score + toInt($(this).val());
+        score = score + parseInt($(this).val());
       });
 
-      if (score <= 0) {
+      if (score <= -2) {
         $("#fortune").append("<h3>You will see a scary clown today.</h3>");
-      } else if ((score > 0) && (score < 5)) {
+      } else if ((score > -2) && (score < 3)) {
         $("#fortune").append("<h3>You will walk through downtown for five whole blocks without being asked for money or a cigarette.</h3>");
       } else {
         $("#fortune").append("<h3>You will find twenty dollars on the ground.</h3>");
